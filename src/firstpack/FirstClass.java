3a7d8e017dcb0232008ffb16811cfb73323554b6
Ibrahim Kalin <hisetkal@gmail.com>
2022-07-17 15:26:29 -0400

    third commit

--- src/firstpack/FirstClass.java
+++ src/firstpack/FirstClass.java
@@ -5,5 +5,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("This is a new update on local master branch");
+        System.out.println("This is third commit from local master");
     }
 }
