844abfa3192d83cf44cc482a2ed1f6784c74e032
Ibrahim Kalin <hisetkal@gmail.com>
2022-07-17 15:24:09 -0400

    second from local master

--- src/firstpack/FirstClass.java
+++ src/firstpack/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("This is a new update on local master branch");
     }
 }
